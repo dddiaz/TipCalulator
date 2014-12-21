7bf2531270f9defaa55e5971ca4e8d33025b3cb3
Daniel Diaz <dddiaz@uci.edu>
2014-12-20 23:04:47 -0800

    Initial Commit

---
+++ TipCalulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalulator
+//
+//  Created by Daniel Diaz on 12/20/14.
+//  Copyright (c) 2014 Daniel Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
