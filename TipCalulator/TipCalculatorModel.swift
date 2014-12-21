10b832e0f0554af36411cffb06e89b6ae62ee6c7
Daniel Diaz <dddiaz@uci.edu>
2014-12-20 23:11:07 -0800

    init commit
    trying to learn swift, init commit

---
+++ TipCalulator/TipCalculatorModel.swift
@@ -0,0 +1,46 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalulator
+//
+//  Created by Daniel Diaz on 12/20/14.
+//  Copyright (c) 2014 Daniel Diaz. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+class TipCalculatorModel {
+    
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+    
+}
